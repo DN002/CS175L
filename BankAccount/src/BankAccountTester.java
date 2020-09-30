5984b2f8e6d775f337ae7a616b3c37d313bfcde7
chris <chris@MSI-DN002>
2020-09-30 15:01:29 -0400

    Completed BankAccount Assignment

---
+++ BankAccount/src/BankAccountTester.java
@@ -0,0 +1,15 @@
+
+
+public class BankAccountTester {
+	
+	public static void main(String[] args) {
+
+		BankAccount myBankAccount = new BankAccount(1000);
+		myBankAccount.withdraw(500);
+		myBankAccount.deposit(1500);
+
+		System.out.println(myBankAccount.getBalance());
+
+	}
+	
+}
