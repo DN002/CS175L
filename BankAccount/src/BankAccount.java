f13505b2f0b7dc3c24e56cd1888af633476c7055
chris <chris@MSI-DN002>
2020-10-05 13:40:22 -0400

    Modified BankAccount and BankAccountTester

--- BankAccount/src/BankAccount.java
+++ BankAccount/src/BankAccount.java
@@ -5,41 +5,49 @@ applying interest or fees at the end of the month.
 */
 public class BankAccount
 {
-private double balance;
-
-/**
-   Constructs a bank account with zero balance.
- * @param amount 
-*/
-public BankAccount(double amount)
-{
-   balance = amount;
-}
+	private double balance;
+	/**
+   	Constructs a bank account with zero balance.
+	* @param amount 
+	*/
+	public BankAccount(double amount)
+	{
+		balance = amount;
+	}
 
 /**
    Makes a deposit into this account.
    @param amount the amount of the deposit
 */
-public void deposit(double amount)
-{
-   balance = balance + amount;
-}
+	public void deposit(double amount)
+	{
+		balance = balance + amount;
+	}
 
 /**
    Makes a withdrawal from this account, or charges a penalty if
    sufficient funds are not available.
    @param amount the amount of the withdrawal
 */
-public void withdraw(double amount)
-{
-   balance = balance - amount;
-}
+	public void withdraw(double amount)
+	{
+		balance = balance - amount;
+		if (balance > 100) 
+		{System.out.println("Withdrawal Completed.");}
+		else {System.out.println("Insufficient Funds to support withdrawal.");}
+	}
 /**
    Gets the current balance of this bank account.
-   @return the current balance
+ * @param newAmt, double interestPct 
 */
-public double getBalance()
-{
-   return balance;
-}
+	
+	public void calcInterest(double amount)
+	{
+		balance = balance + (balance * amount);
+	}
+	
+	public double getBalance()
+	{
+		return balance;
+	}
 }
--- BankAccount/src/BankAccountTester.java
+++ BankAccount/src/BankAccountTester.java
@@ -1,15 +1,35 @@
-
+import java.util.Scanner;
 
 public class BankAccountTester {
 	
 	public static void main(String[] args) {
-
-		BankAccount myBankAccount = new BankAccount(1000);
-		myBankAccount.withdraw(500);
-		myBankAccount.deposit(1500);
-
-		System.out.println(myBankAccount.getBalance());
-
+		
+		Scanner input = new Scanner(System.in);
+		final double interestPct = .05;
+		System.out.print("Enter amount to start your account: ");
+		double amount = input.nextDouble();
+		
+		BankAccount myBankAccount = new BankAccount(amount);
+		System.out.print("Created new account with "+amount +" ");
+		
+		System.out.print("Enter amount to deposit: ");
+		double deposit = input.nextDouble();
+		myBankAccount.deposit(deposit);	
+		System.out.print("Successfully deposited: "+deposit +" ");
+		System.out.println("Balance after deposit: "+myBankAccount.getBalance());
+		
+		
+		System.out.print("Enter amount to withdraw: ");
+		double withdraw = input.nextDouble();
+		myBankAccount.withdraw(withdraw);
+		System.out.print("Successfully withdrawn: "+withdraw +" ");
+		System.out.println("Balance after withdrawn: "+myBankAccount.getBalance());
+		
+		myBankAccount.calcInterest(interestPct);
+		
+		System.out.println("Balance + Calculated Interest: "+myBankAccount.getBalance());
+		
+		input.close();
 	}
 	
 }
